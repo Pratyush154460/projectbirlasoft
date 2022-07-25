caa7b4d692eb7dd0986828a26e32ebd2e3c19ed0
Acer <Acer@DESKTOP-97086LK>
2022-07-25 11:59:35 +0530

    Add project files.

---
+++ cs_con_project2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_con_project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
